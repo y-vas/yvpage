c2c5a4317009ad2cab186f342e3b07d665060e13
Vasyl <yovdiyvasyl@gmail.com>
2019-12-26 12:02:21 +0100

    debug

---
+++ README.md
@@ -0,0 +1,1 @@
+![logo](https://github.com/y-vas/yvpage/blob/master/public_html/assets/images/vylogo.svg)
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yovdiy</title>
+    <link rel="icon" href="../assets/images/vylogo.png">
+    <link href="https://necolas.github.io/normalize.css/8.0.0/normalize.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/index.css">
+  </head>
+
+  <body>
+    <img id="VY" style="display:none;" src="../assets/images/vylogo.svg">
+    <canvas id="floating_city" class="floating_city"> </canvas>
+
+    <div class="content">
+      <div class="wrapper">
+          <div class="txt">
+
+            <h3> Vasyl Yovdiy
+              <br> Let's be honest and cut through the marketing fluff.
+              <br> You need a website or a software project that looks amazing and actually works.
+              <br> Bottom line, that's what I do. And if you want to learn more about working with me, pick up the phone and give me a call (or email).
+            </h3>
+
+          </div>
+
+          <div class="txt2">
+              <h1> <a class="itemo it" href="files/cv.pdf" download> CV </a> <a class="itemo">/</a>
+              <a class="itemo it" href="https://github.com/y-vas"> GitHub </a> </h1>
+          </div>
+
+      </div>
+    </div>
+
+    <div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </body>
+
+  <script src="../assets/js/city.js"></script>
+</html>
