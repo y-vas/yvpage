05e4e0bc353c2fbb3b7ab1d06f044026b2c6fed3
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-01-07 15:47:19 +0100

    debug

--- resources/views/utils/cv.blade.php
+++ resources/views/utils/cv.blade.php
@@ -12,15 +12,3 @@
 		</div>
 
 		</div>
-
-
-		<script type="text/javascript">
-		  function iframeLoaded() {
-		      var iFrameID = document.getElementById('cv');
-		      if(iFrameID) {
-		            // here you can make the height, I delete it first, then I make it again
-		            iFrameID.height = "";
-		            iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-		      }
-		  }
-		</script>
