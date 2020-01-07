efbfab723137d15595269dd46b45b3c134686364
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-01-07 15:46:59 +0100

    cv update

--- resources/views/utils/cv.blade.php
+++ resources/views/utils/cv.blade.php
@@ -4,7 +4,7 @@
 		<div class="col-lg-12">
 
 		<div>
-		  <iframe id="cv" src="/files/CV2020.pdf" style="
+		  <iframe id="cv" src="/files/cv.pdf" style="
 				width:100%;
 				height:100%;
 				border: none;
