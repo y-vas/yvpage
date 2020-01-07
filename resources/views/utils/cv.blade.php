d36c910aab5e4af2dc10ed6324fd5de50bf1a309
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-01-07 13:51:31 +0100

    DEBUG

---
+++ resources/views/utils/cv.blade.php
@@ -0,0 +1,26 @@
+
+
+
+		<div class="col-lg-12">
+
+		<div>
+		  <iframe id="cv" src="/files/CV2020.pdf" style="
+				width:100%;
+				height:100%;
+				border: none;
+			"></iframe>
+		</div>
+
+		</div>
+
+
+		<script type="text/javascript">
+		  function iframeLoaded() {
+		      var iFrameID = document.getElementById('cv');
+		      if(iFrameID) {
+		            // here you can make the height, I delete it first, then I make it again
+		            iFrameID.height = "";
+		            iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+		      }
+		  }
+		</script>
