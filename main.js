658f6c8be0fa75d61896afa56b69133823e8c561
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-12-15 11:51:59 +0100

    added spanish localization

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
     success: function(data){ $('.experiences').html( data ); },
   });
 
-  // start()
+  $.ajax({
+    url: '/localization.json',
+    dataType: "json",
+    success: function(data){
+      data = data[ navigator.language ];
+      try {
+        $('*[trans]').each( function(i, e){
+          let key = $(this).attr('trans')
+          $(this).text(data[key]);
+        });
+      } catch (e) {
+        console.log('language not avaliable');
+      }
 
-  
+    },
+  });
+
 })
