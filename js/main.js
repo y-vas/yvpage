82be0a4d3b62a885b08a4b6d5fa9e838a1056797
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2021-01-24 23:43:01 +0100

    debug

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+var skills_bucket = {}
+
 $(document).ready(function(){
   links = [
     'experiences',
@@ -8,6 +11,7 @@ $(document).ready(function(){
   for (var i = 0; i < links.length; i++) {
     let link = links[i];
     $.ajax({
+      async: false,
       url : `/${link}.html`,
       success: function( data ){ $(`.${link}`).html( data ); },
     });
@@ -16,6 +20,7 @@ $(document).ready(function(){
   $.ajax({
     url: '/localization.json',
     dataType: "json",
+    async: false,
     success: function( data ){
       try {
         $('*[trans]').each( function(i, e){
@@ -27,6 +32,22 @@ $(document).ready(function(){
         console.log('language not avaliable');
       }
     },
+  });
+
+  $('.skills-bucket li').each(function(){
+    let skill = $(this).text().trim().toLowerCase();
+    $(this)
+      .attr('ncss', skill )
+      .mouseover(function(){
+        let skill = $(this).attr('ncss')
+        $(`li[ncss="${skill}"]`).addClass('skill-selected')
+        console.log('hi');
+      })
+      .mouseleave(function(){
+        console.log('ho');
+        let skill = $(this).attr('ncss')
+        $(`li[ncss="${skill}"]`).removeClass('skill-selected')
+      });
   });
 
 })
