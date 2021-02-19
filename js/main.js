107c00b72e4420a8a40469e72f50ef5ea1b8888c
vas <yovdiyvasyl@gmail.com>
2021-02-19 16:24:25 +0100

    debug sub expirinces prev

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 
-var skills_bucket = {}
+var skills_bucket = {
+  'python'     : 'tensorflow, selenium, keras, jupyter, matplotlib ,numpy ,pandas ,pip , pypi',
+  'php'        : 'Composer, PHPMailer, Stripe, Doctrine',
+  'linux'      : 'Bash, Grep, RegEx, awk, vim',
+  'javascript' : ' JQuery, Ajax, select2, d3.js/c3.js , Dragula',
+}
 
 $(document).ready(function(){
   links = [
@@ -36,17 +41,17 @@ $(document).ready(function(){
 
   $('.skills-bucket li').each(function(){
     let skill = $(this).text().trim().toLowerCase();
-    $(this)
+    $( this )
       .attr('ncss', skill )
       .mouseover(function(){
         let skill = $(this).attr('ncss')
-        $(`li[ncss="${skill}"]`).addClass('skill-selected')
-        console.log('hi');
+        $(`li[ncss="${skill}"]`)
+          .addClass('skill-selected')
       })
       .mouseleave(function(){
-        console.log('ho');
         let skill = $(this).attr('ncss')
-        $(`li[ncss="${skill}"]`).removeClass('skill-selected')
+        $(`li[ncss="${skill}"]`)
+          .removeClass('skill-selected')
       });
   });
 
